1be103aed89a79effc78f9f5e6ff99efca948330
Arman Asha <armanhabib353@gmail.com>
2023-09-09 22:16:27 +0600

    new

--- template.py
+++ template.py
@@ -2,3 +2,14 @@ import os
 from pathlib import Path
 import logging
 
+logging.basicConfig(level=logging.INFO)
+
+project_name="mlProject"
+
+
+list_of_file = [
+    ".github/workflows/.gitkeep",
+    f"src/mlProject/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion.py",
+]
