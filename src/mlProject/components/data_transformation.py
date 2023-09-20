5c06ddec781fa03f510308adecd5a9dcb337689a
armanhabib353 <armanhabib353@gmail.com>
2023-09-20 17:36:28 +0600

    new

--- src/mlProject/components/data_transformation.py
+++ src/mlProject/components/data_transformation.py
@@ -3,3 +3,7 @@ from dataclasses import dataclass
 
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
