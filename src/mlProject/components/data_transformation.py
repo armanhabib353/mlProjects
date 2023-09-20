d9eee0773bfea4ed701d614d190bd1c125d928b6
armanhabib353 <armanhabib353@gmail.com>
2023-09-20 17:37:08 +0600

    new

--- src/mlProject/components/data_transformation.py
+++ src/mlProject/components/data_transformation.py
@@ -7,3 +7,7 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
+
+from src.mlProject.exception import CustomException
+from src.mlProject.logger import logging
+import os
