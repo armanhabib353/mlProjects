0571eeb8d14552bd1f2fedb8131307338e21e787
armanhabib353 <armanhabib353@gmail.com>
2023-09-20 17:40:43 +0600

    new

--- src/mlProject/components/data_transformation.py
+++ src/mlProject/components/data_transformation.py
@@ -16,3 +16,7 @@ import os
 @dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformation()
